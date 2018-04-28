2563c48d232bb967e019b868dcd46f4278b56469
Alexander Davis <alexander@davisonline.uk>
2018-04-28 20:12:58 +0100

    Database
    Created connection to database module

---
+++ Exam Buff or Exam Bluff/Exam Buff or Exam Bluff/Data.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Data.SqlClient
+
+Module Data
+    Public Sub SqlQuery()
+        Dim con As New SqlConnection
+        Try
+            con.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\EBEBDat.mdf;Integrated Security=True"
+            Dim cmd As New SqlCommand("SELECT * FROM Questions", con)
+            con.Open()
+            'MessageBox.Show("Connection Opened")
+            Dim QueryResults = New List(Of List(Of Object))
+            'Execute Query
+            Dim reader As SqlDataReader = cmd.ExecuteReader()
+            While reader.Read()
+                Dim rowLength = reader.FieldCount()
+                Dim currentRow = New Object()
+                reader.GetValues(currentRow)
+                QueryResults.Add(currentRow.ToList())
+            End While
+        Catch ex As Exception
+            MessageBox.Show("Error while connecting to SQL Server. " & ex.Message)
+        End Try
+    End Sub
+End Module
