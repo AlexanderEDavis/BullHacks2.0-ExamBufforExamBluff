1fe60769c08cb1d277dc318c968df4ef373cba4e
Lyall Lyall <lyalljohnlyall@hotmail.com>
2018-04-28 20:08:11 +0100

    All UI windows completed
    No logic included - yes buttons on end game/game over dialogs do nothing atm

--- Exam Buff or Exam Bluff/Exam Buff or Exam Bluff/Form2.vb
+++ Exam Buff or Exam Bluff/Exam Buff or Exam Bluff/Form2.vb
@@ -2,4 +2,8 @@
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         lblPassedName.Text = "Hello " + Form1.playerName + "!"
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click, Button5.Click, Button4.Click, Button3.Click, Button1.Click
+        Dialog1.Show()
+    End Sub
 End Class
