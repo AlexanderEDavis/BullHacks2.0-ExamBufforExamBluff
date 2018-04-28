2dcb139417314c792d14a86c7cc721cc6cf416b2
Lyall Lyall <lyalljohnlyall@hotmail.com>
2018-04-28 17:56:38 +0100

    basic UI created
    Name added to game form when entered

---
+++ Exam Buff or Exam Bluff/Exam Buff or Exam Bluff/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Public playerName As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        playerName = ""
+    End Sub
+
+    Private Sub btnNameConfirm_Click(sender As Object, e As EventArgs) Handles btnNameConfirm.Click
+
+        playerName = txtNameEntry.Text
+        Form2.Show()
+        Me.Close()
+    End Sub
+End Class
---
+++ Exam Buff or Exam Bluff/Exam Buff or Exam Bluff/Form2.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblPassedName.Text = "Hello " + Form1.playerName + "!"
+    End Sub
+End Class
