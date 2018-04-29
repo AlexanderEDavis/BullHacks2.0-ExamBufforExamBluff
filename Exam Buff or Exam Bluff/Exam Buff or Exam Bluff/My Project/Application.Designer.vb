a4d41e00559e9f238eecb0fe15c28bbd06186197
Alexander Davis <alexander@davisonline.uk>
2018-04-29 08:57:34 +0100

    Completion of Splash Screen

--- Exam Buff or Exam Bluff/Exam Buff or Exam Bluff/My Project/Application.Designer.vb
+++ Exam Buff or Exam Bluff/Exam Buff or Exam Bluff/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Exam_Buff_or_Exam_Bluff.WelcomeScreen
+            Me.MainForm = Global.Exam_Buff_or_Exam_Bluff.SplashScreen
         End Sub
     End Class
 End Namespace
